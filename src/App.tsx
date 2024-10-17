2ca5e9773db6034b460227d234c547bf8bc48100
BrishnaN <brishna@udel.edu>
2024-10-16 22:38:10 -0400

    Adding title and description for each button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import { Button, Form } from "react-bootstrap";
 import Home from "./components/home";
@@ -31,12 +31,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Ethan Seifer, Brishna Nazari, Shukria Muhammadi</p>
-
         {/* Home navigation Button */}
 
         <Link to="/">
@@ -44,36 +38,31 @@ function App() {
             Home
           </Button>
         </Link>
+      </header>
 
-        {/* Routes for the different pages */}
+      {/* Routes for the different pages */}
+      <div className="App-body">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/basic_question" element={<BasicQuestions />} />
           <Route path="/detailed_question" element={<DetailedQuestions />} />
         </Routes>
+      </div>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <Form>
-        <Form.Label>API Key:</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Insert API Key Here"
-          onChange={changeKey}
-        ></Form.Control>
-        <br></br>
-        <Button className="Submit-Button" onClick={handleSubmit}>
-          Submit
-        </Button>
-      </Form>
+      <footer className="App-footer">
+        <Form>
+          <Form.Label>API Key:</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Insert API Key Here"
+            onChange={changeKey}
+          ></Form.Control>
+          <br></br>
+          <Button className="Submit-Button" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Form>
+      </footer>
     </div>
   );
 }
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,17 +1,42 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to</h1> home page
-      <p>Click the button below to take the assessment.</p>
-      {/* Button to navigate to the assessment page */}
-      <a href="/basic_question">
-        <button>Basic Questions</button>
-      </a>
-      <a href="/detailed_question">
-        <button>Detailed Questions</button>
-      </a>
+    <div className="App-body">
+      <Container>
+        <Row>
+          <Col>
+            <div className="Button-container">
+              <h2 className="Button-title">Basic QUestion</h2>
+              <p className="Button-description">
+                Start with a series of basic questions to assess your knowledge.
+              </p>
+              {/* Button to navigate to the assessment page */}
+              <a href="/basic_question">
+                <Button variant="outline-light" size="lg" className="mt-3">
+                  Basic Questions
+                </Button>
+              </a>
+            </div>
+          </Col>
+
+          <Col>
+            <div className="Button-container">
+              <h2 className="Button-title">Detailed QUestion</h2>
+              <p className="Button-description">
+                Dive deeper into more detailed questions for a thorough
+                assessment.
+              </p>
+              <a href="/detailed_question">
+                <Button variant="outline-light" size="lg" className="mt-3">
+                  Detailed Questions
+                </Button>
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
